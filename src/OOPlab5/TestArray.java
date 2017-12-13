a08ac8a33eb185b176952bd3427cb050f93c8784
LAB_COM-MT_1 <gift.srilamun@gmail.com>
2017-12-13 09:11:10 +0700

    Testarray

---
+++ src/OOPlab5/TestArray.java
@@ -0,0 +1,40 @@
+package OOPlab5;
+//  introduction to array in java
+
+public class TestArray {
+    public static void main (String[] args) {
+        //Array
+        int number[];
+        number = new int[5];
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+        System.out.println(number[2]);
+        System.out.println(number[3]);
+        System.out.println(number[4]);
+        number[1] = 10;
+        System.out.println(number[1]);
+        number[4] = 20;
+        number[3] = number[1] + number[4];
+        System.out.println(number[3]);
+        System.out.println(number[4]);
+
+        int total = 0;
+        for (int i = 0; i < number.length; i++)
+            System.out.println("number[" + i + "]: " + number[i]);
+            total += number[i]; //total in array is "+total);
+
+    }//for
+     System.out.println("total in array is"+total);
+
+    double value [] = new double[5];
+    for (int i=0;i<value.length;i++)
+        system.out.prinln       (value)
+
+
+
+    }//main
+
+
+}//class
+
+
