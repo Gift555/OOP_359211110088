34eb2484f37bc7c349283f6d33a9e800d45a9095
LAB_COM-MT_1 <gift.srilamun@gmail.com>
2018-02-07 11:00:56 +0700

    myPersonApp

--- src/OOPlap9/myPersonApp.java
+++ src/OOPlap9/myPersonApp.java
@@ -2,13 +2,16 @@ package OOPlap9;
 
 public class myPersonApp {
     public static void main(String[] args) {
-        Person person = new Person("1111111111111",
+        Person person = new Person("359211110088",
                 "Ketsarapon Srilamun",
                 new Address("2 M.3", "Phattalung",
                         "93000"),
                 new Job("Student", 4000));
 
         System.out.println(person.toString());
+        person.getJob().setSalary(2000);
+        System.out.println("Name: "+person.getName()+
+                " Salary: "+person.getJob().getSalary());
 
 
     }   //main
